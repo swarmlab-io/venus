c8878cded1ee038e86cda6de4b9e7507cb9b9c7a
Shane <sotiris4321@gmail.com>
2021-05-17 00:43:27 +0300

    clean

--- hybrid/scripts/create-keys.sh
+++
@@ -1,9 +0,0 @@
-
-
-wg genkey | tee ./privatekey | wg pubkey > ./publickey
-
-privatekey=$(head -1 ./privatekey)
-publickey=$(head -1 ./publickey)
-
-jq --arg key0   'private' --arg private $privatekey --arg key1   'public' --arg public $publickey '. | .[$key0]=$private | .[$key1]=$public ' <<<'{}' | tee ./keys.json
-
